78382e8e2feb311deda4d89c5e30a44757e028f4
fallegt-wq <fallegt@gmail.com>
2026-04-07 11:08:19 +0000

    Update Index.js

--- server/Index.js
+++ server/Index.js
@@ -642,6 +642,7 @@ app.post("/api/clubs/:clubSlug/checkout/stripe", async (req, res) => {
       `${FRONTEND_URL}/c/${clubSlug}/checkout?cancelled=true`;
 
     const session = await stripe.checkout.sessions.create({
+     payment_method_types: ["card"],
       mode: "payment",
       customer_email: buyerEmail,
       line_items: [

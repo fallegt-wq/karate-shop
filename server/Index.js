bf70dcdf8f3689ea371a4a1a7ef18b61f1d4cd82
fallegt-wq <fallegt@gmail.com>
2026-04-24 14:04:17 +0000

    Update Index.js

--- server/Index.js
+++ server/Index.js
@@ -131,13 +131,23 @@ function normalizeUrlBase(value) {
 }
 
 function getAppBaseUrl(req) {
-  // ALWAYS use production URL if set
-  if (process.env.FRONTEND_URL) {
-    return process.env.FRONTEND_URL;
+  if (FRONTEND_URL) {
+    return FRONTEND_URL;
   }
 
-  const origin = req.headers.origin;
-  if (origin) return origin;
+  const originHeader = normalizeUrlBase(req.get("origin"));
+  if (originHeader) return originHeader;
+
+  const forwardedProto = normalizeUrlBase(req.get("x-forwarded-proto"));
+  const forwardedHost = normalizeUrlBase(req.get("x-forwarded-host"));
+  const host = normalizeUrlBase(req.get("host"));
+
+  const proto = forwardedProto || req.protocol || (IS_PRODUCTION ? "https" : "http");
+  const hostname = forwardedHost || host;
+
+  if (hostname) {
+    return `${proto}://${hostname}`.replace(/\/$/, "");
+  }
 
   return "http://localhost:5173";
 }
